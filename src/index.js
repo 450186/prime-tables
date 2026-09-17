d04fffa81dbbba6e6749564054cf898d6adf026f
450186 <146966554+450186@users.noreply.github.com>
2026-09-17 12:24:12 +0100

    allow retries for incorrect inputs
    original solution meant re-opening the CLI after an incorrect input

--- src/index.js
+++ src/index.js
@@ -7,18 +7,19 @@ const isValidInput = require("./isValidInput");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
-
-rl.question('How many prime numbers would you like to see? ', (answer) => {
-    const n = Number(answer);
-    if(!isValidInput(n)) {
-        console.log("Please enter a positive integer.");
+});
+function askForInput() {
+    rl.question('How many prime numbers would you like to see? ', (answer) => {
+        const n = Number(answer);
+        if(!isValidInput(n)) {
+            console.log("Please enter a positive integer.");
+            askForInput();
+            return;
+        }
+        const primes = generatePrimes(n);
+        const table = generateTable(primes);
+        console.log(formatTable(table));
         rl.close();
-        return;
-    }
-    const primes = generatePrimes(n);
-    const table = generateTable(primes);
-    console.log(formatTable(table));
-
-    rl.close();
-})
+    });
+}
+askForInput();
